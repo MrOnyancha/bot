ed49a3f3ee596a4706709f84b9643419bdec5854
chrisp22 <onyanchachrispinus@gmail.com>
2017-02-15 14:39:18 +0300

    Adding test

--- app/controllers/MessengerController.scala
+++ app/controllers/MessengerController.scala
@@ -2,8 +2,9 @@ package controllers
 
 import javax.inject.{ Inject, Singleton }
 
-import models._
-import models.Messages._
+
+//import models._
+//import models.Messages._
 import play.api.Configuration
 import play.api.libs.json._
 import play.api.mvc.{ Action, Controller }
@@ -32,32 +33,116 @@ class MessengerController @Inject() (
     }
   }
 
-  def receiveMessage = Action.async(parse.json) { request =>
-    val futures = request.body.as[ReceivedMessage].entry
-      .flatMap(_.messaging)
-      .filter(_.message.isDefined)
-      .map(messaging => messaging.sender -> messaging.message.get)
-      .map { tuple =>
-        val sender = tuple._1
-        val message = tuple._2
-        message.text match {
-          case Messages.commandPattern(subreddit, order) => println(subreddit, order); Future(Json.toJson(Messages.help(sender)))// getRedditPosts(subreddit, order, sender)
-          case _ => Future(Json.toJson(Messages.help(sender)))
+//  def receiveMessage = Action.async(parse.tolerantJson) { request =>
+//    val futures = request.body.as[ReceivedMessage].entry
+//      .flatMap(_.messaging)
+//      .filter(_.message.isDefined)
+//      .map(messaging => messaging.sender -> messaging.message.get)
+//      .map { tuple =>
+//        val sender = tuple._1
+//        val message = tuple._2
+//        message.text match {
+//          case Messages.commandPattern(subreddit, order) => println(subreddit, order); Future(Json.toJson(Messages.help(sender)))// getRedditPosts(subreddit, order, sender)
+//          case _ => Future(Json.toJson(Messages.help(sender)))
+//        }
+//      }
+//      .map(messengerService.reply)
+//    Future.sequence(futures).map(responses => Ok("Finished"))
+//  }
+//
+//  private def getRedditPosts(subreddit: String, order: String, sender: User): Future[JsValue] = {
+//    redditService.getSubreddit(subreddit, order, Some(10)).map {
+//      posts =>
+//        Json.toJson(
+//          StructuredMessage(
+//            recipient = sender,
+//            message = Map("attachment" -> Attachment.from(posts))
+//          )
+//        )
+//    }
+//  }
+//}
+
+
+  def receiveMessage = Action(parse.tolerantJson) { req =>
+    val data = req.body
+
+    (data \ "object").as[String] match {
+      // Make sure this is a page subscription
+      case "page" =>
+        // Iterate over each entry
+        // There may be multiple if batched
+        /**
+          * {{{
+          * {
+          *   "object":"page",
+          *   "entry":[
+          *     {
+          *       "id":"PAGE_ID",
+          *       "time":1458692752478,
+          *       "messaging":[
+          *         {
+          *           "sender":{
+          *             "id":"USER_ID"
+          *           },
+          *           "recipient":{
+          *             "id":"PAGE_ID"
+          *           },
+          *
+          *           ...
+          *         }
+          *       ]
+          *     }
+          *   ]
+          * }
+          * }}}
+          */
+
+        val entries = (data \ "entry").as[List[JsValue]]
+        entries.foreach { pageEntry =>
+          //val pageID = (pageEntry \ "id").as[Long]
+          //val timeOfEvent = (pageEntry \ "time").as[Long]
+
+          val messaging = (pageEntry \ "messaging").as[List[JsObject]]
+          messaging.foreach { messagingEvent =>
+            receivedMessage(messagingEvent)
+          }
         }
-      }
-      .map(messengerService.reply)
-    Future.sequence(futures).map(responses => Ok("Finished"))
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know you've
+        // successfully received the callback. Otherwise, the request will time out.
+        Status(200)
+      case _ =>
+        Status(403)
+    }
   }
 
-  private def getRedditPosts(subreddit: String, order: String, sender: User): Future[JsValue] = {
-    redditService.getSubreddit(subreddit, order, Some(10)).map {
-      posts =>
-        Json.toJson(
-          StructuredMessage(
-            recipient = sender,
-            message = Map("attachment" -> Attachment.from(posts))
-          )
-        )
+  private def receivedMessage(event: JsObject) = {
+    val senderID = (event \ "sender" \ "id").as[String]
+    val recipientID = (event \ "recipient" \ "id").as[String]
+
+    val maybeMessage = (event \ "message").asOpt[JsObject]
+    println {
+      "Received message for user %s and page %s with message: %s".format(senderID, recipientID, maybeMessage)
     }
+
+    maybeMessage.foreach { message =>
+      (message \ "text").asOpt[String].foreach { messageText =>
+        sendTextMessage(senderID, messageText)
+      }
+    }
+  }
+
+  def sendTextMessage(recipientID: String, messageText: String) = {
+    val ACCESS_TOKEN = sys.env("ACCESS_TOKEN")
+
+    ws.url("https://graph.facebook.com/v2.6/me/messages")
+      .withQueryString("access_token" -> ACCESS_TOKEN)
+      .post(Json.obj(
+        "recipient" -> Json.obj("id" -> recipientID),
+        "message" -> Json.obj("text" -> messageText.toUpperCase)
+      ))
   }
 }
